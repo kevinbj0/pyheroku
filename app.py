2bfd141e488a62661df6794df576705aecfa5ae6
ByoungJin Kim <104419151+kevinbj0@users.noreply.github.com>
2024-07-17 11:05:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import requests
 import json
 
+# CSV 파일 경로
+csv_file_path = os.path.join("data", "초급(전자제품)데이터_최종본.csv")
+
+# CSV 파일 읽기
+df = pd.read_csv(csv_file_path)
+pd.set_option('display.max_rows', None)
+print(df.head())
 # Ollama 서버의 ngrok URL
 ngrok_url = "https://8c5d-221-146-182-123.ngrok-free.app"  # 예: "http://your-ngrok-url.ngrok.io"
 
