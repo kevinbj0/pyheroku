94be3ca7e4075e83b0671ee45f2e49adf236a43f
ByoungJin Kim <104419151+kevinbj0@users.noreply.github.com>
2024-07-21 00:01:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
-import os
-import pandas as pd
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    # CSV 파일 경로
-    csv_file_path = os.path.join(os.path.dirname(__file__), "data", "초급(전자제품)데이터_최종본.csv")
-    
-    try:
-        # CSV 파일 읽기
-        df = pd.read_csv(csv_file_path, encoding='utf-8')
-        pd.set_option('display.max_rows', None)
-        return df.head().to_json(orient='records', force_ascii=False)
-    except FileNotFoundError:
-        return jsonify({"error": f"CSV file not found at {csv_file_path}"}), 404
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+import flask
+from flask import request, jsonify
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Hello World</h1>'''
+@app.route("/login",  methods = ['POST'])
+def convert():
+    user = request.get_json()
+    if(user['username'] == 'admin' and user['password'] == '1234'):
+        return jsonify({'success': True, 'message': 'Valid user'})
+    return jsonify({'success': False, 'message': 'Invalid user'})
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
