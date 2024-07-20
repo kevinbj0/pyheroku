67552e75d34dd2e7cdf006d5d9ceaa003c7d82c4
ByoungJin Kim <104419151+kevinbj0@users.noreply.github.com>
2024-07-21 01:18:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
+# Base
 import os
 import openai
 
-# OpenAI API 키 설정
-openai.api_key = os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = "sk-proj-ibW3RoqOrHHfvLGf7hE4T3BlbkFJuAzr1ia2Qbhl2KMTm6Yh"
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 import nest_asyncio
 nest_asyncio.apply()
 
 from llama_index.llms.openai import OpenAI
+# llm=OpenAI(model="gpt-3.5-turbo", temperature=0.1)
 import pandas as pd
 import numpy as np
 from IPython.display import Markdown, display
@@ -18,9 +20,10 @@ from llama_index.experimental.query_engine import PandasQueryEngine
 from llama_index.core import PromptTemplate
 
 from flask import Flask, request, jsonify, Response, make_response
+# from simple_salesforce import Salesforce
 import json
 
-from deep_translator import GoogleTranslator
+# from geopy.geocoders import Nominatim
 from langdetect import detect
 
 from llama_index.core.query_pipeline import (
@@ -36,16 +39,14 @@ from llama_index.core import PromptTemplate
 
 # CSV 파일 경로
 csv_file_path = os.path.join(os.path.dirname(__file__), "data", "초급(전자제품)데이터_최종본.csv")
+    
 
 # CSV 파일 읽기
-try:
-    df = pd.read_csv(csv_file_path, encoding='utf-8')
-    pd.set_option('display.max_rows', None)
-except Exception as e:
-    print(f"Error reading CSV file: {e}")
-    df = pd.DataFrame()  # 빈 데이터프레임 생성
+df = pd.read_csv(csv_file_path, encoding='utf-8')
+pd.set_option('display.max_rows', None)
 
 print(df.head())
+
 
 ft_llm = OpenAI(model="gpt-4o")
 
@@ -58,7 +59,7 @@ instruction_str = (
     "6. Do not quote the expression.\n"
 )
 
-# 시스템 프롬프트
+#시스템
 system_prompt_str = """
     "당신은 대유백화점의 AI 상담원으로 전자제품을 추천하거나 팔고 있습니다.\n"
     "처음 대화를 시작할 때 '안녕하세요 고객님, 대유백화점 AI상담원입니다. 현재 화장품, 전자제품을 취급하고 있습니다. 어떤 품목을 안내해드릴까요?'로 시작해야 한다.\n"
@@ -79,11 +80,21 @@ system_prompt_str = """
     "고객이 제품을 선택하고 제품가격을 질의하면 제품명, df['제품명']에 해당하는 가격, df['가격']를 반환해야 한다.\n"
     "고객이 제품을 선택하고 최종적으로 '결제금액' 또는 '전체금액'을 알려주세요 라고 하면 df['가격']에 고객의 주문 개수, n을 곱하여 안내해야 한다.\n"
     "고객이 주문 취소 관련 요청사항이 없다면 상담을 종료하겠습니다'라고 안내해야합니다.\n"
-    "전자제품에 대한 추천은 기본적으로 df['키워드1']과 df['사양']을 참고해야 하며 예를들어 '출력이 쌘 전자레인지'와 같이 출력,세기에 대한 문의는 df['출력(W)']를, '크기가 큰 냉장고'와 같은 문의는 df['크기(L)']를 참고해야하며 에어컨에서는 '13평정도에 추천할 에어컨 있나요'와 같은 문의는 df['냉방면적(평)']을 참고해서 고객이 말한 평
-"""
+    "전자제품에 대한 추천은 기본적으로 df['키워드1']과 df['사양']을 참고해야 하며 예를들어 '출력이 쌘 전자레인지'와 같이 출력,세기에 대한 문의는 df['출력(W)']를, '크기가 큰 냉장고'와 같은 문의는 df['크기(L)']를 참고해야하며 에어컨에서는 '13평정도에 추천할 에어컨 있나요'와 같은 문의는 df['냉방면적(평)']을 참고해서 고객이 말한 평수에서 근접한 평수에 해당하는 에어컨을 추천해줘야 합니다. 예를들어 에어컨이 9평형 15평형이 있다면 15평형 에어컨을 추천해줘야 합니다.\n"
+    "전자제품에 대한 추천에서 고객이 예를들어 '벽걸이 TV 있나요' 라고 말했을 경우, df['키워드1']를 먼저 참고해야합니다. df[(df['키워드1'].str.contains('벽걸이')) & (df['카테고리'] == 'TV')]으로 벽걸이와 TV에 해당하는 값을 안내하면 됩니다.\n"
+    "전자제품에 대한 추천에서 고객이 예를들어 '사무실에서 쓰기 좋은 노트북 있나요'라고 말했다면, 사무용 노트북을 말하며 df['키워드1']를 먼저 참고해야합니다. df[(df['키워드1'].str.contains('사무용')) & (df['카테고리'] == '노트북')]으로 사무용과 노트북에 해당하는 값을 안내해야되며, 고객이 '성능이 좋은 노트북'과 같이 말한다면 df[(df['키워드1'].str.contains('전문가용')) & (df['카테고리'] == '노트북')]으로 전문가용과 노트북에 해당하는 값을 안내해줘야 하며, 고객이 '휴대하기 좋은 노트북', '휴대성 좋은 노트북'을 찾는다면 가벼운 노트북을 의미하며 df[(df['키워드1'].str.contains('학생용')) & (df['카테고리'] == '노트북')]으로 학생용과 노트북에 해당하는 값을 안내해줘야 합니다.\n"
+    "전자제품에 대한 추천에서 고객이 예를들어 '큰 냉장고'를 찾는다면 df['크기(L)']를 참고해야 하며 df['크기(L)']값이 큰 냉장고를 추천해줘야 합니다. '집이 좀 좁은데 적합한 냉장고 없나요'와 같이 작은 공간을 언급한다면 df['크기(L)'] 값이 작은 냉장고를 추천해주면 됩니다.\n"
+    "전자제품에 대한 추천에서 고객이 예를들어 '사양 비교해주세요'와 같이 비교에 대해 말했을 경우, df['사양']을 참고하여 사양을 비교하고 사양비교에 대한 장단점을 말해줘야 합니다.\n"
+    "주문내역을 출력할때는 목록이나 상세정보 같은 다른사항을 출력하면 안되고 오로지 주문내역만을 출력해줘야 합니다.\n"
+    "당신은 반드시 pandas_engine을 사용하여 주어진 데이터 'df'에 기반하여 답변해야 합니다.\n"
+    """
+# f"""
+# 고객이 '구매할게요','주문할게요', '~개 주세요'와 같이 주문을 하는 말을 했을 경우 제품이 df['특가']=='여름특가'제품인지 조회해보고 만약 '여름특가'제품인 경우 C1)을 실행해야 합니다.\n'여름특가'제품이 아니라면 C1)을 실행하지 않고 주문내역을 출력해주어야 합니다. 주문내역 형식 : '\n================주문내역================\n\n •제품명: df['제품명']\n •브랜드: df['브랜드']\n •결제수량: \n •결제금액: \n df['이미지링크']\n'\n
+# C1)만약 고객이 '여름특가'제품을 주문하는 경우는 주문내역, 상세정보를 출력해선 안되고 다른말 없이 다음과 같이 말해야 합니다.: "해당 제품은 여름특가로 인해 주문이 많아 설치 시기가 늦어질 수 있습니다. 주문을 진행할까요?" 여기서 고객이 "네", "진행할게요", "주문할게요"와 같이 동의하는 답변을 하면, 주문 내역을 출력 후 주문을 마쳐야 합니다. 반면, 고객이 "아니요" 등 반대 의사를 표현하면, 다음과 같이 안내하세요: "네, 고객님. 그렇다면 일주일 내로 설치 가능한 제품들을 추천드리겠습니다." 이때, df['특가']=='여름 특가'가 아닌 에어컨을 추천해야 합니다.\n
+# """
 
 system_prompt = PromptTemplate(system_prompt_str)
-
+    
 pandas_prompt_str = (
     "You are working with a pandas dataframe in Python.\n"
     "The name of the dataframe is `df`.\n"
@@ -115,6 +126,19 @@ pandas_prompt = PromptTemplate(pandas_prompt_str).partial_format(
 )
 pandas_output_parser = PandasInstructionParser(df)
 response_synthesis_prompt = PromptTemplate(response_synthesis_prompt_str)
+# llm = OpenAI(
+#     model="gpt-3.5-turbo",
+#     temperature=0.1,
+# )
+
+# system_prompt="""
+#         당신은 대유의 AI 상담원으로 전자제품, 의류, 화장품을 추천하거나 팔고 있습니다.\n
+#         유의사항1~3의 내용을 그대로 말하지 말고 참고만 해서 답변해야 합니다.\n
+#         처음 대화를 시작할 때 '안녕하세요 대유의 AI상담원입니다.'로 시작해야 합니다.\n
+#         유의사항1) 연령대 컬럼의 숫자들은 연령별 선호도를 뜻합니다.\n
+#         유의사항2) df['성별']은 제품을 선호하는 성별입니다. 예를 들어 '20대 남성이 좋아하는 노트북 있나요'라고 물어보면 df['성별']이 '남성'이고 20대의 선호도가 높은 제품들을 알려주면 됩니다.\n
+#         유의사항3) 상품의 가격을 물어본다면 df['판매가']를 알려주고 df['할인율']이 0보다 크면 할인하는 제품이라고 안내해야 합니다.\n
+#     """
 
 qp = QP(
     modules={
@@ -143,6 +167,8 @@ qp.add_links(
 )
 # add link from response synthesis prompt to llm2
 qp.add_link("response_synthesis_prompt", "llm2")
+# add link from response synthesis prompt to llm2
+qp.add_link("response_synthesis_prompt", "llm2")
 
 def check_exit(request_message):
     exit_keywords = ['exit', 'quit', '종료']
@@ -162,7 +188,7 @@ pipeline_memory.put(system_prompt_add)
 
 app = Flask(__name__)
 
-@app.route('/message', methods=["POST"])
+@app.route('/', methods=["GET","POST"])
 def post_example():
     global pipeline_memory
 
@@ -181,12 +207,10 @@ def post_example():
         print('초기화됨')
         system_prompt_add = ChatMessage(role="system", content=system_prompt)
         pipeline_memory.put(system_prompt_add)
-
-    # 한글로 요청 전송
-    translated_request = GoogleTranslator(source='auto', target='ko').translate(request_message)
+       
 
     # 사용자 입력 메시지를 생성하고 메모리에 추가
-    user_msg = ChatMessage(role="user", content=translated_request)
+    user_msg = ChatMessage(role="user", content=request_message)
     pipeline_memory.put(user_msg)
 
     # 메모리에서 채팅 기록을 가져옴
@@ -202,17 +226,13 @@ def post_example():
     
     response_message = response.message.content
     
-    # 받은 답변 한국어로
-    translated_response = GoogleTranslator(source='auto', target='ko').translate(str(response_message))
-
-    # 응답 메시지를 메모리에 추가
-    response_msg = ChatMessage(role="assistant", content=translated_response)
+     # 응답 메시지를 메모리에 추가
+    response_msg = ChatMessage(role="assistant", content=response_message)
     pipeline_memory.put(response_msg)
 
-    print('답변 : ' + translated_response)
 
     # json 파일로 변환
-    response_json = json.dumps({'message' : translated_response}, ensure_ascii=False)
+    response_json = json.dumps({'message' : response_msg}, ensure_ascii=False)
 
     return response_json
 
